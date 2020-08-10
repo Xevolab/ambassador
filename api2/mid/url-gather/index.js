ea4eee1d24e6675489db528042e2b23294c715f8
cfranc0 <mc.checco2212@gmail.com>
2020-08-10 17:28:41 +0200

    Disabled Puppeteer sandbox

--- api2/mid/url-gather/index.js
+++ api2/mid/url-gather/index.js
@@ -2,7 +2,7 @@
  * @Author: francesco
  * @Date:   2020-06-15T23:51:25+02:00
  * @Last modified by:   francesco
- * @Last modified time: 2020-08-10T16:38:59+02:00
+ * @Last modified time: 2020-08-10T17:12:05+02:00
  */
 
 /**
@@ -19,7 +19,7 @@ module.exports = function (url, params) {
   return new Promise(async (resolve, reject) => {
 
     // Starting a new Puppeteer browser and page
-    const browser = await Puppeteer.launch();
+    const browser = await Puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
 
     // Setting base options
